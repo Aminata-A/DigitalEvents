d0fc1f4dabe96d0b80a2f7e8570617864ec44bb3
ndourmouhammad <ndourmouhammad@gmail.com>
2024-07-05 12:34:26 +0000

    améliorer le style de la maquette

--- resources/views/accueils/accueil.blade.php
+++ resources/views/accueils/accueil.blade.php
@@ -1,3 +1,4 @@
+
 <!DOCTYPE html>
 <html lang="fr">
 <head>
@@ -6,8 +7,13 @@
     <title>Accueil - DigitalEvents</title>
     <!-- Bootstrap CSS -->
     <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    
     <!-- Custom CSS -->
     <style>
+        @import url("https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap");
+        body {
+            font-family: "Montserrat", sans-serif;
+        }
         .navbar-brand {
             font-weight: bold;
             color: #333; /* Couleur du texte du logo */
@@ -16,7 +22,7 @@
             background-image: url('{{ asset('img/vert_minimaliste_evenement_printemps_banniere_1.png') }}'); /* Chemin vers votre image de bannière */
             background-size: cover;
             background-position: center;
-            padding: 30px 0;
+            padding: 20px 0;
             text-align: center;
             color: #fff; /* Couleur du texte sur l'image */
         }
@@ -56,149 +62,193 @@
         }
         .card {
             width: 100%;
-            
+
         }
         .events .cards-container {
             display: flex;
             flex-wrap: wrap;
             justify-content: space-around;
-            
-            
-            
+
+
+
             /* margin-right: 280px; */
         }
-        
-        
-        
-        
+
+
+
+
         .types-section {
             margin-top: 50px;
             padding: 50px 0;
-            
+
         }
-        
+
         .types-section .content {
             display: flex;
             flex-wrap: wrap;
-            
+
             align-items: center;
             justify-content: center;
         }
-        
+
         .types-section .content .image {
             flex: 1;
             padding-right: 20px;
         }
-        
+
         .types-section .content .text {
             flex: 1;
             padding-left: 20px;
         }
-        
-        
+
+
         .types-section img {
             width: 100%;
             height: auto;
             border-radius: 10px;
         }
         .text h2 {
-            padding-top: 100px;
+            /* padding-top: 100px; */
         }
-        @media (max-width: 768px) {
-            .types-section .content {
-                flex-direction: column; /* Passer à la disposition en colonne pour les petits écrans */
-            }
-            
-            .types-section .image, .types-section .text {
-                flex-basis: 100%; /* Les éléments prennent toute la largeur disponible en mode colonne */
-                margin-bottom: 20px; /* Espacement entre les éléments en mode colonne */
-            }
+
+        .bouton {
+            border: 1px solid #FF8200;
+            background: #FF8200;
+            color: white
+        }
+
+        .carousel-item2 img {
+            max-height: 100px;
+            object-fit: contain;
+        }
+        .carousel2 {
+            max-width: 100%;
+            margin: auto;
         }
         
+@media (max-width: 768px) {
+    .types-section .content {
+        flex-direction: column; /* Passer à la disposition en colonne pour les petits écrans */
+    }
+
+    .types-section .image, .types-section .text {
+        flex-basis: 100%; /* Les éléments prennent toute la largeur disponible en mode colonne */
+        margin-bottom: 20px; /* Espacement entre les éléments en mode colonne */
+    }
+}
+
     </style>
 </head>
 <body>
-    
-    
+
+
     <!-- resources/views/components/header.blade.php -->
-    @include('components.headerEvenement')
-    
-    
+<header>
+    <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <a class="navbar-brand" href="#">DigitalEvents</a>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav mr-auto px-4">
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('accueil')}}">Accueil</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="{{ route('evenement') }}" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">Événement</a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item" href="{{ route('evenement') }}">Événement</a>
+                        @auth
+                            @if (Auth::user()->hasRole('association'))
+                                <a class="dropdown-item" href="{{ route('creation') }}">Création d'Événement</a>
+                                <a class="dropdown-item" href="{{ route('mes-evenements') }}">Mes Événements</a>
+                            @endif
+                        @endauth
+                    </div>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('reservations.index')}}">Reservations</a>
+                </li>
+                @auth
+                <li class="nav-item">
+
+                    <a class="nav-link" href="{{ route('logout') }}">Déconnexion</a>
+                </li>
+                @else
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ route('login') }}">Connexion</a>
+                </li>
+                @endauth
+            </ul>
+            <!-- Affichage de l'utilisateur connecté -->
+            @auth
+            <span class="navbar-text">
+                {{ Auth::user()->name }}
+
+            </span>
+            @endauth
+        </div>
+    </nav>
+</header>
+
+
     <!-- Banner -->
     <div class="banner">
         <div class="container-fluid">
             <img src="{{ asset('images/Vert_Minimaliste.png') }}" alt="Bannière" class="img-fluid">
         </div>
     </div>
-    
+
     <!-- À propos de nous -->
     <div class="about-us">
-        <div class="container">
+        <div class="container mt-4">
             <h2>À propos de nous</h2>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus fermentum massa eu dolor varius, et semper lectus aliquam. Nulla tincidunt mauris sit amet erat pretium, sit amet lobortis urna tincidunt. Nulla facilisi.
-                Integer volutpat dignissim lectus, ac dapibus lorem euismod ut. Vestibulum eu magna ac velit condimentum posuere. Vivamus sagittis nisi non risus commodo, a consectetur felis malesuada.
-                Integer volutpat dignissim lectus, ac dapibus lorem euismod ut. Vestibulum eu magna ac velit condimentum posuere. Vivamus sagittis nisi non risus commodo, a consectetur felis malesuada
-                Integer volutpat dignissim lectus, ac dapibus lorem euismod ut. Vestibulum eu magna ac velit condimentum posuere. Vivamus sagittis nisi non risus commodo, a consectetur felis malesuada
-                Integer volutpat dignissim lectus, ac dapibus lorem euismod ut. Vestibulum eu magna ac velit condimentum posuere. Vivamus sagittis nisi non risus commodo, a consectetur felis malesuada
-            </p>
+            Integer volutpat dignissim lectus, ac dapibus lorem euismod ut. Vestibulum eu magna ac velit condimentum posuere. Vivamus sagittis nisi non risus commodo, a consectetur felis malesuada.
+        </p>
         </div>
     </div>
-    
+
     {{-- événements à venir --}}
-    <!-- Événements à venir -->
-    
-    <div id="eventCarousel" class="carousel slide" data-ride="carousel" data-interval="5000">
+    <div id="eventCarousel" class="carousel slide mt-4 container" data-ride="carousel" data-interval="2000">
         <div class="carousel-inner">
             @foreach ($evenements->chunk(3) as $index => $eventChunk)
-            <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
-                <div class="row gx-0 justify-content-center">
-                    @foreach ($eventChunk as $evenement)
-                    <div class="col-md-3">
-                        <div class="card mb-3 mx-2">
-                            <img src="{{ asset('images/image_24.png') }}" class="card-img-top" alt="Image Événement">
-                            <div class="card-body">
-                                <h5 class="card-title">{{ $evenement->name }}</h5>
-                                <p class="card-text">{{ Str::limit($evenement->description, 150) }}</p>
-                                <p class="card-text"><small class="text-muted">Dernière mise à jour: {{ $evenement->updated_at->diffForHumans() }}</small></p>
-                                <a href="{{ route('evenements.show', ['id' => $evenement->id]) }}" class="btn btn-primary">Voir plus</a>
-{{-- =======
-                        <div class="col-md-3">
-                            <div class="card mb-3 mx-2">
-                                <img src="{{ asset('images/image_24.png') }}" class="card-img-top" alt="Image Événement">
-                                <div class="card-body">
-                                    <h5 class="card-title">{{ $evenement->name }}</h5>
-                                    <p class="card-text">{{ Str::limit($evenement->description, 150) }}</p>
-                                    <p class="card-text"><small class="text-muted">Dernière mise à jour: {{ $evenement->updated_at->diffForHumans() }}</small></p>
-                                    <a href="{{ route('evenements.show', ['id' => $evenement->hash_id]) }}" class="btn btn-primary">Voir plus</a>
-                                    
+                <div class="carousel-item {{ $index == 0 ? 'active' : '' }}">
+                    <div class="row justify-content-center">
+                        @foreach ($eventChunk as $evenement)
+                            <div class="col-xs-12 col-sm-6 col-md-4 col-lg-3 mb-3">
+                                <div class="card" style="max-width: 600px; height: 420px;">
+                                    <img src="{{ asset('storage/' . $evenement->image) }}" class="card-img-top" style="height: 150px; object-fit: cover;" alt="Image Événement">
+                                    <div class="card-body text-center">
+                                        <h5 class="card-title">{{ $evenement->name }}</h5>
+                                        <p class="card-text">{{ Str::limit($evenement->description, 100) }}</p>
+                                        <a href="{{ route('evenements.show', ['id' => $evenement->hash_id]) }}" class="btn bouton">Voir plus</a>
+                                    </div>
                                 </div>
->>>>>>> 43e2a04a6837be32997abecdcd497fbde8e3e9e0 --}}
                             </div>
-                        </div>
+                        @endforeach
                     </div>
-                    @endforeach
                 </div>
-            </div>
             @endforeach
         </div>
         <a class="carousel-control-prev" href="#eventCarousel" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="carousel-control-prev-icon text-primary"><img src="{{ asset('images/gauche.svg') }}" alt=""></span>
             <span class="sr-only">Previous</span>
         </a>
         <a class="carousel-control-next" href="#eventCarousel" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="carousel-control-next-icon text-primary"><img src="{{ asset('images/droite.svg') }}" alt=""></span>
             <span class="sr-only">Next</span>
         </a>
     </div>
     
     
-    
-    
-</div>
+
 <!-- Nos types d'événements Section -->
 <section class="types-section">
     <div class="container">
-        <h2 class="text">Nos types d'événements</h2>
+        <h2 class="text mb-3">Nos types d'événements</h2>
         <div class="content">
             <div class="image">
                 <img src="{{ asset('images/image_22.png') }}" class="img-fluid rounded-start event-img" alt="Type d'événement">
@@ -209,7 +259,7 @@
             </div>
         </div>
     </div>
-    
+
     <div class="container">
         <div class="content">
             <div class="text">
@@ -234,32 +284,44 @@
     </div>
 </section>
 <!-- Nos partenaires -->
-<section class="partners-section">
-    <div class="container">
-        <h2>Nos Partenaires</h2>
-        <div class="row">
-            <div class="col-md-3 col-sm-6">
-                <img src="{{ asset('images/image 25.png') }}" class="img-fluid partner-logo" alt="Partenaire 1">
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <img src="{{ asset('images/image 26.png') }}" class="img-fluid partner-logo" alt="Partenaire 2">
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <img src="{{ asset('images/image 27.png') }}" class="img-fluid partner-logo" alt="Partenaire 3">
-            </div>
-            <div class="col-md-3 col-sm-6">
-                <img src="{{ asset('images/image 28.png') }}" class="img-fluid partner-logo" alt="Partenaire 4">
-            </div>
-            
-        </div>
-        
-    </div>
-</section>
 
-<!-- Bootstrap JavaScript et dépendances -->
-<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
-<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
+<div class="container mt-3">
+    <h2>Nos partenaires</h2>
+    <div id="partnerCarousel" class="carousel slide" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="d-flex justify-content-center">
+                    <img src="{{ asset('images/image28.png') }}" alt="Placeholder Image 1">
+                    <img src="{{ asset('images/image27.png') }}" alt="Placeholder Image 2">
+                    <img src="{{ asset('images/image26.png') }}"  alt="Placeholder Image 3">
+                    
+                </div>
+            </div>
+            <!-- Additional carousel items can be added here if you have more images -->
+        </div>
+       
+
+        <a class="carousel-control-prev" href="#partnerCarousel" role="button" data-slide="prev">
+            <span class=" text-primary"><img src="{{ asset('images/gauche.svg') }}" alt=""></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#partnerCarousel" role="button" data-slide="next">
+            <span class=" text-primary"><img src="{{ asset('images/droite.svg') }}" alt=""></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>
+</div>
+
+
+
+
+    <!-- Bootstrap JavaScript et dépendances -->
+    
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.9.2/dist/umd/popper.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.bundle.min.js"></script>
 
 </body>
 </html>
